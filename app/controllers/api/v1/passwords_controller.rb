0b72616353112aa98c388bacb4fab220519f95d9
hamid-murambiwa <machipisajunior@gmail.com>
2022-07-18 16:24:53 +0200

    Add controller for the password recovery process

---
+++ app/controllers/api/v1/passwords_controller.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+module Api
+  module V1
+    class PasswordsController < ApplicationController
+      def forgot
+        return render json: { error: 'Email was not located in the database' } if params[:email].blank?
+
+        user = User.find_by(email: params[:email])
+
+        if user.present?
+          user.generate_password_token!
+          PasswordMailer.with(user:).reset_email.deliver_now
+          render json: { status: 'ok' }, status: :ok
+        else
+          render json: { error: ['Email address not found. Please check and try again.'] }, status: :not_found
+        end
+      end
+
+      def reset
+        token = params[:token].to_s
+
+        return render json: { error: 'Token not present' } if token.blank?
+
+        user = User.find_by(reset_password_token: token)
+
+        if user.present? && user.password_token_valid?
+          if user.reset_password!(params[:password])
+            PasswordConfirmationMailer.with(user:).reset_confirmation.deliver_now
+            render json: { status: 'ok' }, status: :ok
+          else
+            render json: { error: user.errors.full_messages }, status: :unprocessable_entity
+          end
+        else
+          render json: { error: ['Link not valid or expired. Try generating a new link.'] }, status: :not_found
+        end
+      end
+    end
+  end
+end
