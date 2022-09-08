7d9528cb5877711cbf4f173537bc6717ad5aef2d
hamid murambiwa <machipisajunior@gmail.com>
2022-09-08 11:51:15 +0200

    add pg gem

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,3 @@
 #FIXME replace 1.2.3.4 with your IP address
-server '127.0.0.1', user: 'recipe_app', roles: %w{web app db}
+server '52.14.253.105', user: 'deploy', roles: %w{web app db}
 set :rails_env, 'production'
